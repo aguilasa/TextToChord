b56e4c213253529c5c33999a81dcd81fcb4d3d84
Ingmar Schmidt de Aguiar <ingmar.aguiar@gmail.com>
2024-07-19 13:19:08 -0300

    Refactoring

--- src/com/aguilasa/texttochord/Main.java
+++ src/com/aguilasa/texttochord/Main.java
@@ -5,52 +5,48 @@
  */
 package com.aguilasa.texttochord;
 
-import static com.aguilasa.texttochord.model.Consts.PATH_FILES;
+import com.aguilasa.texttochord.utils.ProcessFile;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 
-import com.aguilasa.texttochord.utils.ProcessFile;
-import org.apache.commons.io.FileUtils;
+import static com.aguilasa.texttochord.model.Consts.PATH_FILES;
 
 /**
  * @author Ingmar.Aguiar
  */
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		String path = args.length > 0 ? args[0] : PATH_FILES;
+        String path = args.length > 0 ? args[0] : PATH_FILES;
 
-//		ProcessFile process2 = new ProcessFile();
-//		process2.process(new File(path.concat("\\Gra�a soberana;Paulo C�sar Baruk;F;f.txt")));
+        StringBuilder sb = new StringBuilder();
+        Collection<File> listFiles = FileUtils.listFiles(new File(path), new String[]{"txt"}, false);
+        for (File file : listFiles) {
+            if (file.getName().equalsIgnoreCase("base.txt")) {
+                continue;
+            }
+            ProcessFile process = new ProcessFile(path.concat("\\out\\"));
+            process.saveText = false;
+            process.saveJson = true;
+            process.process(file);
+            sb.append(process.getSongFileChords().toJson(4));
+            sb.append(",");
+        }
 
-		StringBuilder sb = new StringBuilder();
-		Collection<File> listFiles = FileUtils.listFiles(new File(path), new String[] { "txt" }, false);
-		for (File file : listFiles) {
-			if (file.getName().equalsIgnoreCase("base.txt")) {
-				continue;
-			}
-			ProcessFile process = new ProcessFile(path.concat("\\out\\"));
-			process.saveText = false;
-			process.saveJson = true;
-			process.process(file);
-			sb.append(process.getSongFileChords().toJson(4));
-			sb.append(",");
-		}
+        try {
+            FileUtils.write(new File(path.concat("\\saida.json")), sb.toString(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		try {
-			FileUtils.write(new File(path.concat("\\saida.json")), sb.toString(), Charset.forName("UTF-8"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
+    }
 
 }
--- src/com/aguilasa/texttochord/model/Consts.java
+++ src/com/aguilasa/texttochord/model/Consts.java
@@ -5,11 +5,7 @@
  */
 package com.aguilasa.texttochord.model;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Ingmar.Aguiar
